368df3f1410e155f835dc2a0be24c7949ac243d9
salguento <salguento@gmail.com>
2025-11-18 17:44:22 -0300

    add database tables draft

--- src-tauri/src/commands/database_commands.rs
+++ src-tauri/src/commands/database_commands.rs
@@ -19,8 +19,281 @@ pub fn create_tables(state: State<AppState>) -> Result<(), String> {
 
     db.execute(
         "CREATE TABLE IF NOT EXISTS library_paths (
-            id   INTEGER PRIMARY KEY,
+            path_id   INTEGER PRIMARY KEY,
             path TEXT
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS settings (
+            settings_id   INTEGER PRIMARY KEY,
+            cross_fade BOOL DEFAULT TRUE,
+            cross_fade_duration INTEGER DEFAULT 5,
+            normalize_volume BOOL DEFAULT TRUE,
+            explicit_content BOOL DEFAULT TRUE,
+            autoplay BOOL DEFAULT TRUE,
+            preferred_audio_quality INTEGER,
+            preferrend_audio_source INTEGER,
+            download_path TEXT,
+            open_on_startup BOOL DEFAULT FALSE,
+            minimize_on_close BOOL DEFAULT FALSE
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS audio_quality (
+            id   INTEGER PRIMARY KEY,
+            value TEXT
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS audio_sources (
+            id   INTEGER PRIMARY KEY,
+            name TEXT
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS genres (
+            genre_id   INTEGER PRIMARY KEY,
+            name TEXT,
+            description TEXT,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS playlists (
+            playlist_id   INTEGER PRIMARY KEY,
+            name TEXT,
+            artwork TEXT
+
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS songs (
+            song_id   INTEGER PRIMARY KEY,
+            title TEXT,
+            artist_id INTEGER NOT NULL,
+            release_id INTEGER NOT NULL,
+            track_number INTEGER NOT NULL,
+            disc_numbert INTEGER NOT NULL DEFAULT 1,
+            duration INTEDGER NOT NULL,
+            preferred_file_path TEXT,
+            preferred_file_format INTEGER,
+            preferred_file_size INTEGER,
+            bitrate INTEGER,
+            sample_rate INTEGER,
+            play_count INTEGER DEFAULT 0,
+            last_played DATETIME,
+            rating INTEGER,
+            lyrics TEXT,
+            is_favorite BOOL DEFAULT FALSE,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS playlist_songs (
+            playlist_id   INTEGER NOT NULL,
+            song_id INTEGER NOT NULL,
+            position INTEGER NOT NULL,
+            added_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (playlist_id, song_id),
+            FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id) ON DELETE CASCADE,
+            FOREIGN KEY (song_id) REFERENCES songs(song_id) ON DELETE CASCADE
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS artists (
+            id   INTEGER PRIMARY KEY,
+            name TEXT,
+            image TEXT,
+            bio TEXT,
+            formed_year INTEGER,
+            birthplace TEXT,
+            is_favorite BOOL,
+            spotify_url TEXT,
+            apple_music_url TEXT,
+            discogs_url TEXT,
+            last_fm_url TEXT,
+            rate_your_music_url TEXT,
+            current_location TEXT,
+            birthday DATETIME
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS release_types (
+            release_type_id   INTEGER PRIMARY KEY,
+            name TEXT
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS labels (
+            id   INTEGER PRIMARY KEY,
+            name TEXT,
+            country TEXT
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS releases (
+            id   INTEGER PRIMARY KEY,
+            title TEXT,
+            artist_id INTEGER NOT NULL,
+            artists_name TEXT,
+            release_type_id INTEGER NOT NULL,
+            release_date DATETIME,
+            total_tracks INTEGER DEFAULT 1,
+            total_discs INTEGER DEFAULT 1,
+            label_id INTEGER NOT NULL,
+            artwork TEXT,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            description TEXT,
+            is_explicit BOOL,
+            is_favorite BOOL,
+            rating INTEGER,
+            spotify_url TEXT,
+            apple_music_url TEXT,
+            last_fm_url TEXT,
+            discog_url TEXT,
+            rate_your_music_url TEXT,
+            FOREIGN KEY (artist_id) REFERENCES artists(artist_id) ON DELETE CASCADE,
+            FOREIGN KEY (release_type_id) REFERENCES release_types(release_type_id) ON DELETE CASCADE
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS artists_releases (
+            id   INTEGER PRIMARY KEY,
+            title TEXT,
+            artist_id INTEGER NOT NULL,
+            artists_name TEXT,
+            release_type_id INTEGER NOT NULL,
+            release_date DATETIME,
+            total_tracks INTEGER DEFAULT 1,
+            total_discs INTEGER DEFAULT 1,
+            label_id INTEGER NOT NULL,
+            artwork TEXT,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            description TEXT,
+            is_explicit BOOL,
+            is_favorite BOOL,
+            rating INTEGER,
+            spotify_url TEXT,
+            apple_music_url TEXT,
+            last_fm_url TEXT,
+            discog_url TEXT,
+            rate_your_music_url TEXT,
+            FOREIGN KEY (artist_id) REFERENCES artists(artist_id) ON DELETE CASCADE,
+            FOREIGN KEY (release_type_id) REFERENCES release_types(release_type_id) ON DELETE CASCADE
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS artists_releases (
+            id   INTEGER PRIMARY KEY,
+            artist_id INTEGER NOT NULL,
+            release_id INTEGER NOT NULL,
+            FOREIGN KEY (artist_id) REFERENCES artists(artist_id) ON DELETE CASCADE,
+            FOREIGN KEY (release_id) REFERENCES releases(release_id) ON DELETE CASCADE
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS artists_playlists (
+            id   INTEGER PRIMARY KEY,
+            artist_id INTEGER NOT NULL,
+            playlist_id INTEGER NOT NULL,
+            FOREIGN KEY (artist_id) REFERENCES artists(artist_id) ON DELETE CASCADE,
+            FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id) ON DELETE CASCADE
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS labels_artists (
+            id   INTEGER PRIMARY KEY,
+            artist_id INTEGER NOT NULL,
+            label_id INTEGER NOT NULL,
+            FOREIGN KEY (artist_id) REFERENCES artists(artist_id) ON DELETE CASCADE,
+            FOREIGN KEY (label_id) REFERENCES labels(label_id) ON DELETE CASCADE
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS play_history (
+                history_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                song_id INTEGER NOT NULL,
+                played_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                play_duration INTEGER,
+                FOREIGN KEY (song_id) REFERENCES songs(song_id) ON DELETE CASCADE
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS file_format (
+            id   INTEGER PRIMARY KEY,
+            name TEXT
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS sample_rate (
+            id   INTEGER PRIMARY KEY,
+            value TEXT
+        )",
+        (),
+    )
+    .map_err(|e| format!("Failed to create table: {}", e))?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS bitrate (
+            id   INTEGER PRIMARY KEY,
+            value TEXT
         )",
         (),
     )
@@ -79,3 +352,5 @@ pub fn get_paths_from_library_paths(state: State<AppState>) -> Result<Vec<Librar
     let paths: Result<Vec<LibraryPath>, _> = path_iter.collect();
     paths.map_err(|e| format!("Failed to collect results: {}", e))
 }
+
+
