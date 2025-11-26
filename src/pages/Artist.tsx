9e3826178fe272d9755a043306d8a8b978b77788
salguento <salguento@gmail.com>
2025-11-26 12:30:17 -0300

    add backbtn to artist dynamic page

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -2,6 +2,7 @@
 import { OverlayScrollbarsComponent } from "overlayscrollbars-solid";
 import { For } from "solid-js";
 import { A } from "@solidjs/router";
+import BackButton from "../ui/components/BackButton";
 
 interface SongInfo {
   track: number;
@@ -79,11 +80,7 @@ export default function Album() {
             class="backdrop-blur-3xl flex flex-row items-center gap-2 pt-3 px-4 pb-3 w-full h-full "
             data-tauri-drag-region
           >
-            <button class="hover:cursor-pointer h-6 w-6 text-zinc-400 hover:text-white">
-              <A href="/">
-                <span class="icon-[solar--arrow-left-linear] h-6 w-6 "></span>
-              </A>
-            </button>
+            <BackButton />
             <span class="text-xl font-semibold">Artist</span>
           </div>
         </div>
