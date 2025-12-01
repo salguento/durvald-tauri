54586524c022cfa57da152f3a50086e4d1862cb2
salguento <salguento@gmail.com>
2025-12-01 15:05:54 -0300

    remove rodio error struct definitions

--- src-tauri/src/audio/mod.rs
+++ src-tauri/src/audio/mod.rs
@@ -1,6 +1,5 @@
 pub mod player;
 pub mod queue;
-pub mod types;
 
 pub use player::AudioPlayer;
 // pub use queue::Playlist;
--- src-tauri/src/audio/types.rs
+++ src-tauri/src/audio/types.rs
@@ -1,27 +1,6 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum AudioError {
-    #[error("Audio stream error: {0}")]
-    StreamError(#[from] rodio::StreamError),
-
-    #[error("Audio device error: {0}")]
-    DeviceError(#[from] rodio::DevicesError),
-
-    #[error("IO error: {0}")]
-    IoError(#[from] std::io::Error),
-
-    #[error("Decoder error: {0}")]
-    DecoderError(#[from] rodio::decoder::DecoderError),
-}
-
-#[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum PlaybackState {
     Playing,
     Paused,
     Stopped,
 }
-
-#[allow(dead_code)]
-pub type Result<T> = std::result::Result<T, AudioError>;
