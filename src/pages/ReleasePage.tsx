32cf8bd77ea008add7d577ef860bc857311f99c7
salguento <salguento@gmail.com>
2025-12-03 14:25:24 -0300

    update ReleasePage Styling

--- src/pages/ReleasePage.tsx
+++ src/pages/ReleasePage.tsx
@@ -1,6 +1,6 @@
 // Dependencies
 import { OverlayScrollbarsComponent } from "overlayscrollbars-solid";
-import { For, onMount } from "solid-js";
+import { For, onMount, Show } from "solid-js";
 import { useParams } from "@solidjs/router";
 import { invoke } from "@tauri-apps/api/core";
 import { createSignal } from "solid-js";
@@ -30,12 +30,39 @@ export default function ReleasePage() {
     <div class="bg-zinc-900 size-7/12 h-full rounded-3xl grow overflow-hidden border border-zinc-700/50">
       <div class="relative w-full shadow-xl">
         <div class="absolute  w-full text-white  bg-zinc-900/50  z-1  border-b border-zinc-700/50">
-          <div
-            class="backdrop-blur-3xl flex flex-row items-center gap-2 pt-3 px-4 pb-3 w-full h-full "
-            data-tauri-drag-region
-          >
-            <BackButton />
-            <span class="text-xl font-semibold">Album</span>
+          <div class="backdrop-blur-3xl grid grid-cols-5 items-center  pt-3 px-4 pb-3 w-full h-full">
+            <div
+              class="flex items-center gap-2 col-span-2"
+              data-tauri-drag-region
+            >
+              <BackButton />
+              <span class="text-xl font-semibold">Album</span>
+            </div>
+            <div
+              class="col-span-1 flex gap-4 items-center justify-center"
+              data-tauri-drag-region
+            >
+              <button class="w-8 h-8">
+                <span class="icon-[solar--play-circle-bold] w-8 h-8 text-white"></span>
+              </button>
+              <button class="w-6 h-6">
+                <span class="icon-[solar--shuffle-linear] w-6 h-6 text-white"></span>
+              </button>
+            </div>
+            <div
+              class="col-span-2 flex gap-4 items-center justify-end"
+              data-tauri-drag-region
+            >
+              <button class="w-6 h-6">
+                <span class="icon-[solar--add-square-linear] w-6 h-6 text-white"></span>
+              </button>
+              <button class="w-6 h-6">
+                <span class="icon-[solar--heart-angle-linear] w-6 h-6 text-white"></span>
+              </button>
+              <button class="w-6 h-6">
+                <span class="icon-[solar--menu-dots-bold] w-6 h-6 text-white"></span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -50,61 +77,69 @@ export default function ReleasePage() {
         defer
         class="w-full flex flex-col gap-4 h-full"
       >
-        <div class="flex flex-col gap-8 w-full pt-18 relative">
-          <div class="flex flex-row gap-8 items-center px-4">
-            <div>
-              <img
-                src={release()?.artwork}
-                class="h-48 w-48 rounded-2xl"
-                alt=""
-              />
-            </div>
-            <div class="flex flex-col gap-1">
-              <span class="text-2xl text-white font-semibold">
-                {release()?.title}
-              </span>
-              <A href={`/artist/${release()?.artist_id}`}>
-                <span class="text-xl text-zinc-400 font-medium hover:underline hover:cursor-pointer">
-                  {release()?.artist_name}
+        <div class="h-96 relative flex items-center">
+          <div class="absolute w-full h-full  overflow-hidden flex items-center">
+            <div class="bg-zinc-900/50 backdrop-blur-3xl absolute w-full h-full"></div>
+            <img src={release()?.artwork}></img>
+          </div>
+          <div class="flex flex-col gap-8 w-full relative pt-14">
+            <div class="flex flex-row gap-8 items-center px-4">
+              <div>
+                <img
+                  src={release()?.artwork}
+                  class="h-48 w-48 rounded-2xl"
+                  alt=""
+                />
+              </div>
+              <div class="flex flex-col gap-1">
+                <span class="text-2xl text-white font-semibold">
+                  {release()?.title}
                 </span>
-              </A>
-              <div class="flex flex-rol gap-2">
-                <span class="text-md text-zinc-600 font-medium hover:underline hover:cursor-pointer">
-                  Electronic
-                </span>
-                <span class="text-md text-zinc-600 font-medium">•</span>
-                <span class="text-md text-zinc-600 font-medium hover:underline hover:cursor-pointer">
-                  {release()?.release_date}
-                </span>
+                <A href={`/artist/${release()?.artist_id}`}>
+                  <span class="text-xl text-white/35 font-semibold hover:underline hover:cursor-pointer mix-blend-plus-lighter">
+                    {release()?.artist_name}
+                  </span>
+                </A>
+                <div class="flex flex-rol gap-2">
+                  <span class="text-md text-zinc-400 font-medium hover:underline hover:cursor-pointer">
+                    Electronic
+                  </span>
+                  <span class="text-md text-zinc-400 font-medium">•</span>
+                  <span class="text-md text-zinc-400 font-medium hover:underline hover:cursor-pointer">
+                    {release()?.release_date}
+                  </span>
+                  <Show when={release()?.duration}>
+                    <span class="text-md text-zinc-400 font-medium">•</span>
+                    <span class="text-md text-zinc-400 font-medium hover:underline hover:cursor-pointer">
+                      {msToMinSec(release()!.duration)}
+                    </span>
+                  </Show>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div>
           <div class="  shadow overflow-hidden">
             <table class="w-full divide-y divide-zinc-700">
-              <thead class="">
-                <tr>
-                  <th class="px-2 py-3 text-left text-xs font-medium text-zinc-500 uppercase tracking-wider"></th>
-                  <th class="px-2 py-3 text-left text-xs font-medium text-zinc-500 uppercase tracking-wider">
-                    #
-                  </th>
-                  <th class="px-6 py-3 text-left text-xs font-medium text-zinc-500 uppercase tracking-wider">
-                    Name
-                  </th>
-                  <th class="px-6 py-3 text-left text-xs font-medium text-zinc-500 uppercase tracking-wider">
-                    Duration
-                  </th>
+              <thead class="h-4 text-[10px] font-medium text-zinc-500 uppercase tracking-wider">
+                <tr class="">
+                  <th class="w-8  text-left py-1"></th>
+                  <th class="w-8 text-center py-1">#</th>
+                  <th class="px-2  text-left py-1">Title</th>
+                  <th class="px-2 text-left  py-1">Duration</th>
                 </tr>
               </thead>
               <tbody class="bg-zinc-900 divide-y divide-zinc-700">
                 <For each={songs()}>
                   {(song: SongType) => (
                     <tr
-                      class="hover:bg-zinc-700 "
+                      class="hover:bg-zinc-700 group hover:cursor-pointer relative h-8"
                       onDblClick={() => playBack(song)}
                     >
-                      <td class="px-4 py-2 whitespace-nowrap">
+                      <td class=" flex justify-center items-center px-2 w-fit  overflow-hidden h-8">
                         <button
-                          class="text-zinc-400 hover:text-white hover:cursor-pointer"
+                          class="text-zinc-400 hover:text-white hover:cursor-pointer  flex justify-center w-4"
                           title={`${
                             song.is_favorite
                               ? "Unfavorite song"
@@ -112,20 +147,29 @@ export default function ReleasePage() {
                           }`}
                         >
                           <span
-                            class={`w-4 h-4 rounded-full mr-3 ${
+                            class={`w-4 h-4 rounded-full  ${
                               song.is_favorite
                                 ? "icon-[solar--heart-bold]"
-                                : "icon-[solar--heart-linear]"
+                                : "group-hover:icon-[solar--heart-linear]"
                             }`}
                           ></span>
                         </button>
                       </td>
-                      <td class="px-2 py-2 whitespace-nowrap">
-                        <div class="text-sm font-medium text-zinc-400">
-                          {song.track_number}
+                      <td class="whitespace-nowrap items-center">
+                        <div class="flex items-center justify-center w-8">
+                          <div class="text-sm font-medium text-zinc-400 group-hover:hidden">
+                            {song.track_number}
+                          </div>
+                          <button
+                            class="group-hover:block hidden h-4 w-4 hover:cursor-pointer"
+                            title="Play"
+                            onClick={() => playBack(song)}
+                          >
+                            <span class="icon-[solar--play-bold] text-white h-4 w-4"></span>
+                          </button>
                         </div>
                       </td>
-                      <td class="px-6 py-2 whitespace-nowrap">
+                      <td class="px-2  whitespace-nowrap">
                         <div class="flex items-center">
                           <div class="text-sm font-medium text-zinc-400">
                             {song.title}
@@ -143,7 +187,7 @@ export default function ReleasePage() {
                           {song.isFavorite ? "Folder" : song.isAdded || "File"}
                         </span>
                       </td>*/}
-                      <td class="px-6 py-2 whitespace-nowrap text-sm text-zinc-400">
+                      <td class="px-6  whitespace-nowrap text-sm text-zinc-400">
                         {msToMinSec(song.duration)}
                       </td>
                     </tr>
