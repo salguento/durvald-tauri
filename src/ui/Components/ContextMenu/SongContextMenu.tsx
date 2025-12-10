edcd5caf5a0d63ca684b0210f2b79d96d6e13526
salguento <salguento@gmail.com>
2025-12-10 18:00:47 -0300

    update songContextMenu

--- src/ui/Components/ContextMenu/SongContextMenu.tsx
+++ src/ui/Components/ContextMenu/SongContextMenu.tsx
@@ -20,44 +20,85 @@ export default function ContextMenuComponent(props: Props) {
       </ContextMenu.Trigger>
       <ContextMenu.Portal>
         <ContextMenu.Content class="min-w-3xs p-1 bg-black/50 backdrop-blur-3xl border border-zinc-500/50  text-zinc-400 rounded-2xl z-100 outline-none cursor-pointer">
-          <ContextMenu.Item class="hover:bg-zinc-900 relative px-2 py-1 flex justify-between rounded-xl">
-            <div class="">{props.track.title}</div>
+          <ContextMenu.Sub overlap gutter={-8} shift={8}>
+            <ContextMenu.SubTrigger class="flex justify-between items-center cursor-default hover:bg-zinc-800 px-2 py-1 rounded-xl hover:text-white ">
+              <div class="">Add to playlist</div>
+              <span class="icon-[solar--alt-arrow-right-linear] w-4 h-4"></span>
+            </ContextMenu.SubTrigger>
+            <ContextMenu.Portal>
+              <ContextMenu.SubContent class="bg-black/50 backdrop-blur-3xl border border-zinc-500/50 rounded-2xl z-100 overflow-hidden p-1 w-64">
+                <ContextMenu.Item class="text-zinc-400 hover:text-white px-2 py-1 hover:bg-zinc-800/50 rounded-xl cursor-pointer">
+                  Find a playlist
+                </ContextMenu.Item>
+                <ContextMenu.Item class="text-zinc-400 hover:text-white px-2 py-1 hover:bg-zinc-800/50 rounded-xl cursor-pointer">
+                  New playlist
+                </ContextMenu.Item>
+                <ContextMenu.Separator class=" h-px my-1 border-t border-zinc-800/50" />
+                <ContextMenu.Item class="text-zinc-400 hover:text-white px-2 py-1 hover:bg-zinc-800/50 rounded-xl cursor-pointer">
+                  Playlist for loop
+                </ContextMenu.Item>
+              </ContextMenu.SubContent>
+            </ContextMenu.Portal>
+          </ContextMenu.Sub>
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div class="">Add to library</div>
             <div class="">⌘+K</div>
           </ContextMenu.Item>
-          <ContextMenu.Item class="hover:bg-zinc-900 relative px-2 py-1 flex justify-between rounded-xl">
-            <div>Push</div>
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Favorite</div>
             <div>⇧+⌘+K</div>
           </ContextMenu.Item>
-          <ContextMenu.Item
+          <ContextMenu.Separator class="h-px my-1 border-t border-zinc-700/50" />
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Add next to queue</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Add last to queue</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Create song station</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Separator class="h-px my-1 border-t border-zinc-700/50" />
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Hide song</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Suggest less</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Separator class="h-px my-1 border-t border-zinc-700/50" />
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Share</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Info</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Separator class="h-px my-1 border-t border-zinc-700/50" />
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>View source</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Search for sources</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          <ContextMenu.Item class="hover:bg-zinc-800/50 relative px-2 py-1 flex justify-between rounded-xl hover:text-white">
+            <div>Download</div>
+            <div>⇧+⌘+K</div>
+          </ContextMenu.Item>
+          {/*<ContextMenu.Item
             class=" text-zinc-700 relative px-2 py-1 flex justify-between rounded-xl cursor-default"
             disabled
           >
             Update Project <div class="">⌘+T</div>
           </ContextMenu.Item>
-          <ContextMenu.Sub overlap gutter={8} shift={-8}>
-            <ContextMenu.SubTrigger class="flex justify-between cursor-default">
-              <div class="">GitHub</div>
-              <span class="icon-[solar--alt-arrow-right-linear] w-4 h-4"></span>
-            </ContextMenu.SubTrigger>
-            <ContextMenu.Portal>
-              <ContextMenu.SubContent class="bg-black/50 backdrop-blur-3xl border border-zinc-500/50 rounded-2xl z-10 overflow-hidden p-1">
-                <ContextMenu.Item class="text-zinc-400 hover:text-white px-2 py-1 hover:bg-zinc-900 rounded-xl">
-                  Create Pull Request…
-                </ContextMenu.Item>
-                <ContextMenu.Item class="text-zinc-400 hover:text-white px-2 py-1 hover:bg-zinc-900 rounded-xl">
-                  View Pull Requests
-                </ContextMenu.Item>
-                <ContextMenu.Item class="text-zinc-400 hover:text-white px-2 py-1 hover:bg-zinc-900 rounded-xl">
-                  Sync Fork
-                </ContextMenu.Item>
-                <ContextMenu.Separator class=" h-px my-1 border-t border-zinc-500/50" />
-                <ContextMenu.Item class="text-zinc-400 hover:text-white px-2 py-1 hover:bg-zinc-900 rounded-xl">
-                  Open on GitHub
-                </ContextMenu.Item>
-              </ContextMenu.SubContent>
-            </ContextMenu.Portal>
-          </ContextMenu.Sub>
-          <ContextMenu.Separator class="" />
+
           <ContextMenu.CheckboxItem
             class="flex justify-start items-center gap-1"
             checked={showGitLog()}
@@ -101,7 +142,7 @@ export default function ContextMenuComponent(props: Props) {
                 develop
               </ContextMenu.RadioItem>
             </ContextMenu.RadioGroup>
-          </ContextMenu.Group>
+          </ContextMenu.Group>*/}
         </ContextMenu.Content>
       </ContextMenu.Portal>
     </ContextMenu>
